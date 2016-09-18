22a1d41c89bb6ceb3206b2d403fa8a6c081a5dce
Manish <m-bakshi@hotmail.com>
2016-09-18 14:20:19 +0530

    Lecture 10 finished

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-(function(){
+(function () {
 'use strict';
 
-angular.module('NameCalculator', [])
-.controller('NameCalculatorController', function($scope){
- $scope.name = "";
- $scope.totalValue = 0;
- 
- $scope.displayNumeric = function(){
-   var totalNameValue = calculateNumericForString($scope.name); 
-   $scope.totalValue = totalNameValue;
- };
- 
- function calculateNumericForString(string){
-   var totalStringValue = 0;
-   for(var i = 0; i < string.length; i++){
-     totalStringValue += string.charCodeAt(i);
-   }
-   
-   return totalStringValue;
- }
- 
-});
+angular.module('DIApp', [])
+.controller('DIController', DIController);
+
+DIController.$inject = ['$scope','$filter'];
+
+function DIController ($scope, $filter){
+  $scope.name = "Manish";
+
+  $scope.upper = function() {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };}
 
 })();
